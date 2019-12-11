264368849a38bc15350540bd5425e86c794f45e1
stvik <stvik2@gmail.com>
2019-12-11 12:34:36 -0500

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,51 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+addErrorClass()
+document.addEventListener('DOMContentLoaded', () => {
+  addEventListenersToLikes(activateLikes)
+})
 
+function addErrorClass() {
+  modal = document.getElementById("modal")
+  modal.className = "hidden"
+}
+
+
+function addEventListenersToLikes(action) {
+  let likeBtns = document.getElementsByClassName("like-glyph")
+  for (let i = 0; i < likeBtns.length; i++) {
+    likeBtns[i].addEventListener("click", action)
+  }
+}
+
+function activateLikes(event) {
+  mimicServerCall()
+  .then(function() {
+    let heart = event.target
+    heart.innerText = FULL_HEART
+    heart.className = "activated-heart"
+    heart.removeEventListener('click', activateLikes)
+    heart.addEventListener('click',deactivateLikes)
+    console.log('success!')
+  })
+  .catch(function(error){
+    modal = document.getElementById("modal")
+    modal.className = null
+    setTimeout(addErrorClass, 5000)
+    console.log('oops')
+  })
+
+  function deactivateLikes(event){
+    let heart = event.target
+    heart.innerText = EMPTY_HEART
+    heart.className = null
+    heart.removeEventListener('click', deactivateLikes)
+    heart.addEventListener('click', activateLikes)
+    console.log('unliked!')
+  }
+
+}
 
 
 
